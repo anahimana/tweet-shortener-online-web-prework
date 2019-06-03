1cac956d24f8633e9cd76e48c1cd194157e73385
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-03 13:10:34 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,7 +20,7 @@ end
 sample_str = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
 
 def word_substituter(str)
-str_arr = str.split
+# str_arr = str.split
 new_str_arr = []
 str_arr.each {
   |word| dictionary(word) ? new_str_arr << dictionary(word) : new_str_arr << word
