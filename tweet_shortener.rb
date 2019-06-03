1557eefddbaaa47210eb931dca324a48ca4bbadb
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-03 13:09:12 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,8 +18,6 @@ def dictionary(word)
 end
 
 def word_substituter(str)
-sample_str = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
-
 str_arr = str.split
 new_str_arr = []
 str_arr.each {
