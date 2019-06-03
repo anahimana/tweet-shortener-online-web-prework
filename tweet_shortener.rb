c34d4b61f893d9b9fed93c58a8e7c5e2f06ddc79
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-03 12:55:12 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+# Define dictionary method
+# Method takes a string and it's corresponding value from a hash of words
+def dictionary(word)
+  words = {
+    "hello": 'hi',
+    "to": '2',
+    "two": '2', 
+    "too": '2',
+    "for": '4',
+    "be": 'b',
+    "you": 'u',
+    "at": '@',
+    "and": '&'
+  }
+  words[word.to_sym]
+end
+
+sample_str = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+
+puts sample_str.split
